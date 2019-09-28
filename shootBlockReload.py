375a87cc6da98f16238bdabb6ef8721effdfdb97
Jack Tilley <47064848+Jack-Tilley@users.noreply.github.com>
2019-09-28 15:00:03 -0400

    Update shootBlockReload.py

--- shootBlockReload.py
+++ shootBlockReload.py
@@ -4,26 +4,28 @@ class Player:
     1 = SHOOT
     2 = BLOCK
     3 = RELOAD
-    4 = DEFEATED
     -1 = MISFIRE"""
     def __init__(self, number):
         self.number = number
         self.bullets = 0
         self.status = 0  # ready
-        self.target = 0 # no one
-        self.lives = 3
+        self.target = self  # no one
+        self.lives = 1
         self.round_wins = 0
         self.round_losses = 0
+        self.is_living = True
 
     def shoot(self):
         if self.bullets >= 1:
             self.bullets -= 1
-            print("Player {} has chosen to shoot player {}!".format(self.number,self.target))
+            print("Player {} has chosen to shoot player {}!".format(self.number, self.target.number))
         else:
-            print("Player {} has no bullets to shoot player {} with!".format(self.number, self.target))
+            print("Player {} has no bullets to shoot player {} with!".format(self.number, self.target.number))
+            self.status = -1
+            self.flinch()
 
     def block(self):
-        print("Player {} has chosen to block player {}!".format(self.number,self.target))
+        print("Player {} has chosen to block player {}!".format(self.number,self.target.number))
 
     def reload(self):
         self.bullets += 1
@@ -32,19 +34,37 @@ class Player:
     def flinch(self):
         print("Player {} has hesitated for too long!".format(self.number))
 
-    def is_shot(self):
+    def is_shot(self, shooter):
         self.lives -= 1
-        if self.lives <= 0:
-            print("Player {} has been defeated!".format(self.number))
-        else:
+        if self.lives > 0:
+            print("Player {} has been shot by player {}".format(self.number, shooter))
             print("Player {} has {} lives remaining!".format(self.number, self.lives))
+        else:
+            print("Player {} has been eliminated by player {}".format(self.number, shooter))
+
 
     def is_blocked(self):
-        print("Player {} has blocked a shot by {}!".format(self.number, self.target))
+        print("Player {}'s shot has been blocked by player {}!".format(self.number, self.target.number))
 
-    # def has_been_defeated(self):
-    #     if self.lives == 0:
-    #         print("Player {} has been defeated!".format(self.number))
+
+    def is_countered(self):
+        print("Player {}'s bullets have collided with someone else's".format(self.number, self.target.number))
+
+    def is_foolish(self):
+        print("Player {} almost shot themselves!".format(self.number))
+
+    def is_crazy(self):
+        print("Player {} has tried to shoot the dead!".format(self.number))
+
+    def is_rotting(self):
+        print("Player {} is rotting!".format(self.number))
+
+    def is_alive(self):
+        if self.lives <= 0:
+            self.is_living = False
+            return False
+        return True
+
 
     def reset(self):
         self.__init__(self.number)
@@ -53,9 +73,15 @@ class Player:
 class Game:
     def __init__(self, players):
         self.players = players
+        self.living_players = players
+        self.survivor_count = len(players)
+        self.shooting_targets = {}
+        self.blocking_targets = {}
+        self.game_over = False
 
         for player in self.players:
             print("Player {} is ready to battle!".format(player.number))
+
 
     def player_option(self, player):
         print("What will player {} do?".format(player.number))
@@ -65,49 +91,115 @@ class Game:
         else:
             return choice
 
-    def player_target(self, player): # this is the one that is broken, always retyrns -1
+    def player_target(self, player): # this is the one that is broken, always returns -1
         print("Who will player {} face?".format(player.number))
-        choice = input()
-        for player_number in self.players:
-            if player_number.number == choice and player_number.number != player.number:
-                return choice
-        return -1
+        choice = int(input())
+        if choice >= 0 and choice < len(self.players):
+            return choice
+        else:
+            return -1
 
-    def start_round(self):
+    def start_round(self, round_number):
+        player_no = 0
+        print("Round {}. Start!".format(round_number))
         for player in self.players:
-            player.status = self.player_option(player)
-            if len(self.players) > 2:
-                player.target = self.player_target(player)
-            else:
-                for player_number in self.players:
-                    if player.number != player_number.number:
-                        player.target = player_number.number
+            if player.is_alive():
+                player.status = self.player_option(player)
+                if len(self.players) > 2:
+                    if player.status == 1 or player.status == 2:
+                        target_number = self.player_target(player)
+                        if target_number == -1:
+                            player.status = -1
+                            player.target = player
+                        else:
+                            player.target = self.players[target_number]
+                    else:
+                        player.target = player
+                else:
+                    if player_no == 0:
+                        player.target = self.living_players[1]
+                        player_no = 1
+                    else:
+                        player.target = self.living_players[0]
+
+
+
+    def display_round(self):
+        for player in self.players:
+            if player.is_alive():
+                self.event_handler(player)
+
+    def event_handler(self, player):
+        if player.status == 1:
+            player.shoot()
+        elif player.status == 2:
+            player.block()
+        elif player.status == 3:
+            player.reload()
+        else:
+            player.flinch()
 
     def evaluate_round(self):
+
         for player in self.players:
-            if player.status == 1:
-                player.shoot()
-            elif player.status == 2:
-                player.block()
-            elif player.status == 3:
-                player.reload()
+            if player.status == 1 and player.target.is_alive():
+                if not player.is_living:
+                    player.is_rotting()
+                elif player == player.target:
+                    player.is_foolish()
+                elif player.target.status == 1 and player.target.target == player:
+                    player.is_countered()
+                elif player.target.status == 2 and player.target.target == player:
+                    player.is_blocked()
+                else:
+                    player.target.is_shot(player.number)
+
+            elif not player.target.is_alive() and player.status == 1:
+                player.is_crazy()
+
+    def elimination_check(self):
+        self.survivor_count = len(self.players)
+        alive_players = []
+        for player in self.players:
+            if not player.is_alive():
+                self.survivor_count -= 1
+                player.status = -1
+                player.target = player
             else:
-                player.flinch()
+                alive_players.append(player)
+        if len(alive_players) == 1:
+            print("Victory belongs to player {}! Game over.".format(alive_players[0].number))
+            self.game_over = True
+        elif len(alive_players) == 0:
+            print("None remain. Game over.")
+            self.game_over = True
 
-        # maybe make this 2 funcs
+    def game_over_check(self):
+        if self.game_over:
+            return True
+        return False
 
-        for player in self.players:
-            if player.status == 1:
-                if self.players[player.target].status != 2: # this may return -1, causing unnecesary damage
-                    self.players[player.target].is_shot()
-            elif player.status == 2:
-                if self.players[player.target].status == 1: # this may return -1, causing unnecesary damage
-                    self.players[player.target].is_blocked()
+    def reset(self):
+        self.__init__(self.players)
 
-
-
-
-
+    def play(self):
+        i = 0
+        while not self.game_over:
+            self.start_round(i)
+            self.display_round()
+            self.evaluate_round()
+            self.elimination_check()
+            self.game_over = self.game_over_check()
+            i += 1
+        print("Do you want to play again? Y/N")
+        ans = input()
+        if ans.upper() == "Y":
+            for player in self.players:
+                player.reset()
+            self.reset()
+            self.play()
+        else:
+            return 0
 
 
 jack = Player(0)
@@ -116,6 +208,5 @@ sam = Player(2)
 
 
 game1 = Game([jack,serena,sam])
-for i in range(3):
-    game1.start_round()
-    game1.evaluate_round()
+game1.play()
+
